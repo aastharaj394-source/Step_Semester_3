5110875b11aa9b4fc8116f7894981a0728a3e1a3
Aastha Raj <aastharaj394@gmail.com>
2026-09-20 18:14:00 +0530

    ADD reference caopies and shared id card

---
+++ src/main/java/class_problems/Reference_copies_shared_id_card.java
@@ -0,0 +1,28 @@
+package class_problems;
+import java.util.Scanner;
+public class Reference_copies_shared_id_card {
+    class IdCard {
+        String name;
+        int booksIssued;
+
+        public IdCard(String name, int booksIssued) {
+            this.name = name;
+            this.booksIssued = booksIssued;
+        }
+    }
+
+    public class IdCardComparison {
+        public static void main(String[] args) {
+            IdCard ravi = new IdCard("Ravi", 0);
+            IdCard duplicate = ravi;
+
+            duplicate.booksIssued = 3;
+
+            IdCard separate = new IdCard("Ravi", 3);
+
+            System.out.println("Ravi's booksIssued (via first variable): " + ravi.booksIssued);
+            System.out.println("duplicate == ravi: " + (duplicate == ravi));
+            System.out.println("separate == ravi: " + (separate == ravi));
+        }
+    }
+}
